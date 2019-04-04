d7f1392db216ce96062bf808504e80920240e519
raian <munsurtarik@gmail.com>
2019-04-04 14:53:37 +0600

    Routes added

--- raian/routes/index.js
+++ raian/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/second_page', function(req, res, next) {
+  res.render('second_page');
+});
+
+router.get('/third_page', function(req, res, next) {
+  res.render('third_page');
+});
+
 module.exports = router;
